616404c613faf216e7bdd7e63ce87d668dc79992
Serge van den Oever <Serge.van.den.Oever@macaw.nl>
2018-12-17 18:09:29 +0100

    Fixed Dockerfile and configuration

--- Web.App/DockerRunWebAppHttp.bat
+++ Web.App/DockerRunWebAppHttp.bat
@@ -1,1 +1,1 @@
-docker run -dt -e "ASPNETCORE_ENVIRONMENT=Production" -e "ASPNETCORE_URLS=http://+:80" -p 80:80 -p 443:443 -p 8080:8080 webapp:latest
+docker run --rm -i -e "ASPNETCORE_ENVIRONMENT=Production" -e "ASPNETCORE_URLS=http://+:80" -p 80:80 -p 8080:8080 webapp:latest
--- Web.App/Program.cs
+++ Web.App/Program.cs
@@ -18,6 +18,15 @@ namespace Web.App
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            // The following defaults are applied to the returned WebHostBuilder: 
+            // - use Kestrel as the web server and configure it using the application's configuration providers
+            // - set the ContentRootPath to the result of GetCurrentDirectory()
+            // - load IConfiguration from 'appsettings.json' and 'appsettings.[EnvironmentName].json'
+            // - load IConfiguration from User Secrets when EnvironmentName is 'Development' using the entry assembly
+            // - load IConfiguration from environment variables
+            // - configures the ILoggerFactory to log to the console and debug output
+            // - enables IIS integration (how about Linux? - no IIS)
+            // - enables the ability for frameworks to bind their options to their default configuration sections
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
     }
